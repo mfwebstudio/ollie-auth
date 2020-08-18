24aa923bc9f776d0062d1deb8cb93b7f7aa0fbc3
Mayke <mayke_sargento@hotmail.com>
2020-08-18 20:07:13 -0300

    adding main files

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,26 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { Auth } from 'pages/Auth';
+import 'styles/global.css';
+import 'styles/tailwind.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const App: React.FC = () => (
+  <Router>
+    <div>
+      <Switch>
+        <Route path="/auth">
+          <Auth />
+        </Route>
+        <Route path="/">
+          <Home />
+        </Route>
+      </Switch>
     </div>
-  );
-}
+  </Router>
+);
 
 export default App;
+
+function Home() {
+  return <h2>Home</h2>;
+}
