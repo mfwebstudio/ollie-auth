ff9eb808bfe8d7218e7305b21afd9898b949fec5
Mayke <mayke_sargento@hotmail.com>
2020-08-19 17:26:03 -0300

    adding global types

---
+++ src/types/general.ts
@@ -0,0 +1,9 @@
+export type User = {
+  id?: string;
+  password: string;
+  category: string;
+  firstName: string;
+  lastName: string;
+  gender: string;
+  email: string;
+};
---
+++ src/types/signup.ts
@@ -0,0 +1,6 @@
+import { FormikProps } from 'formik';
+import { User } from './general';
+
+export type SignUpStepProps = {
+  form: FormikProps<User>;
+};
